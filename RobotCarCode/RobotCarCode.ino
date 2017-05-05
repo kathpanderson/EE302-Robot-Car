70db570a57c1156515c41f102c9211f131778ed9
Katherine Anderson <Katherine Anderson>
2017-05-05 11:06:44 -0500

    80% code here.

--- RobotCarCode/RobotCarCode.ino
+++ RobotCarCode/RobotCarCode.ino
@@ -3,25 +3,33 @@ Code to control the robot car.
 */
 
   // Set ports for motor outputs
-  const int motorLeft = 5;
-  const int motorRight = 2;
+  const int motorLeft = 9;
+  const int motorRight = 10;
+
+  // Drive number
+  int drive = 51;
 
     // Set ports for photoresistor sensor input
-  const int leftColorPort = A1;
-  const int rightColorPort = A0;
-  const int middleColorPort = A2;
+  const int leftColorPort = 1;
+  const int rightColorPort = 0;
+  const int middleColorPort = 2;
   
   // Set ports for IR sensor input
-  const int leftIRPort = A3;
-  const int rightIRPort = A5;
-  const int middleIRPort = A4;
+  const int leftIRPort = 3;
+  const int rightIRPort = 5;
+  const int middleIRPort = 4;
 
     // Initialize run duration for motors, as well as white/black voltage cut off
   // and distance to stop at
-  int duration = 0; // Update after testing
-  int BlacktoWhite = 3; //White < 3V, black > 3V
-  int redToWhite = 2; // White is > 2V, red < 2V
+  int duration = 100; // Update after testing
+  int turnDuration = 250;
+  int BlacktoWhite = 155; //White < 3V, black > 3V
+  int redToWhite = 1.5; // White is < 1.5V, red > 1.5V
   int stopDistance = 0; // Update after testing
+
+  int rightColorVal = 0;
+  int leftColorVal = 0;
+  int middleColorVal = 0;
 
 void setup() {
 
@@ -29,9 +37,9 @@ void setup() {
   // const int enableOutput = 
 
     Serial.begin(9600);
-    pinMode(2, OUTPUT);
-    pinMode(5, OUTPUT);
-    
+    pinMode(motorLeft, OUTPUT);
+    pinMode(motorRight, OUTPUT);
+   
     // write with digital
     // read will be analog
 }
@@ -41,80 +49,111 @@ void loop() {
 
   // Initilize both motors to low. Motors will always default to low, unless turned on by some part of the program.
   // Once a motor is turned on, it MUST be turned off when the loop breaks.
-  digitalWrite(motorRight, LOW);
-  digitalWrite(motorLeft, LOW);
+  analogWrite(motorRight, 0);
+  analogWrite(motorLeft, 0);
 
   //  SWITCH TO LOW BEFORE YOU RUN ACTUAL CODE, HIGH ONLY FOR TESTING!!!!!!!!!!!!!!
     
   // Loop 1: Follow the sensors on lines. Break upon distance from wall. 
   
-  while(analogRead(middleIRPort) < stopDistance){
+  while(true){
+
+     rightColorVal = analogRead(rightColorPort);
+     leftColorVal = analogRead(leftColorPort);
+     middleColorVal = analogRead(middleColorPort);
     
     // Subloop 1: Both sensors see white. On this, continue forward for duration.
-    while(analogRead(rightColorPort) < BlacktoWhite and analogRead(leftColorPort) < BlacktoWhite){
+    
+    if((rightColorVal <= BlacktoWhite and leftColorVal <= BlacktoWhite)){
       
       // Write HIGH output to both motors
-      digitalWrite(motorRight, HIGH);
-      digitalWrite(motorLeft, HIGH);
-      
-      // Wait with motors on.
+      analogWrite(motorRight, drive);
+      analogWrite(motorLeft, drive);
+
       delay(duration);
+  
+      analogWrite(motorRight, 0);
+      analogWrite(motorLeft, 0);
+
+      delay(duration);
+
+      rightColorVal = analogRead(rightColorPort);
+      leftColorVal = analogRead(leftColorPort);
+      middleColorVal = analogRead(middleColorPort);
+            
+    }
+
+   if(rightColorVal >= BlacktoWhite and leftColorVal >= BlacktoWhite){
+      
+      // Write HIGH output to both motors
+      analogWrite(motorRight, drive);
+      analogWrite(motorLeft, drive);
+
+      delay(duration);
+  
+      analogWrite(motorRight, 0);
+      analogWrite(motorLeft, 0);
+
+      delay(duration);
+
+      rightColorVal = analogRead(rightColorPort);
+      leftColorVal = analogRead(leftColorPort);
+      middleColorVal = analogRead(middleColorPort);
     }
 
    // If condiiton met to break loop, turn off both motors
-    digitalWrite(motorRight, LOW);
-    digitalWrite(motorLeft, LOW);
     
     // Subloop 2: Left sensor sees black. Right sensor sees white.
-    while(analogRead(rightColorPort < BlacktoWhite) and analogRead(leftColorPort > BlacktoWhite)){
-      
-      // Condition 1: Middle sensor sees white. This is normal left turn.
-      while(analogRead(middleColorPort) < BlacktoWhite){
+    if(rightColorVal <= BlacktoWhite and leftColorVal > BlacktoWhite){
         
-        digitalWrite(motorRight, HIGH);
-        
+        analogWrite(motorRight, drive);
+
+        delay(turnDuration);
+
+        analogWrite(motorRight, 0);
+        analogWrite(motorLeft, 0);
+
         delay(duration);
+
+        rightColorVal = analogRead(rightColorPort);
+        leftColorVal = analogRead(leftColorPort);
+        middleColorVal = analogRead(middleColorPort);
     }
-
-      // If condition to exit loop met, turn off right motor
-      digitalWrite(motorRight, LOW);
-        
-      // Condition 2: Middle sensor sees black. This is the right angle turn condition!
-        // Left side off. Right side on. For duration. 
-      while(analogRead(middleColorPort) > BlacktoWhite){
-
-        digitalWrite(motorRight, HIGH);
-
-        delay(duration);
-      }
 
       // If condition met to exit loop, turn off right motor.
-      digitalWrite(motorRight, LOW);
       
     // Subloop 3: Left sensor sees white. Right sensor sees black.
-    while(analogRead(leftColorPort) < BlacktoWhite and analogRead(rightColorPort) > BlacktoWhite){
-      
-      // Condition 1: Middle sensor sees white. This is normal right turn.
-      while(analogRead(middleColorPort) < BlacktoWhite){
-        digitalWrite(motorLeft, HIGH);
+    if(leftColorVal <= BlacktoWhite and rightColorVal > BlacktoWhite){
+       
+        analogWrite(motorLeft, drive);
+
+        delay(turnDuration);
+
+        analogWrite(motorRight, 0);
+        analogWrite(motorLeft, 0);
+
         delay(duration);
-      }
-      digitalWrite(motorLeft, LOW);
-      
-      // Condition 2: Middle sensor sees black. This is right angle turn to right condition!
-      while(analogRead(middleColorPort) > BlacktoWhite){
-        digitalWrite(motorLeft, HIGH);
-        delay(duration);
-      }
-      digitalWrite(motorLeft, LOW);
     }
+
   }
-  }
+  
   
   while(analogRead(middleIRPort) > stopDistance){
   // Loop 2: Delay until wall removed
     delay(duration);
   }
+
+  while(analogRead(middleColorPort) > BlacktoWhite or analogRead(rightColorPort) > BlacktoWhite or analogRead(leftColorPort) > BlacktoWhite){
+    analogWrite(motorLeft, drive);
+    analogWrite(motorRight, drive);
+    delay(duration);
+
+    analogWrite(motorLeft, 0);
+    analogWrite(motorRight, 0);
+  }
+
+  analogWrite(motorLeft, 0);
+  analogWrite(motorRight, 0);
   
   while(analogRead(middleColorPort) < redToWhite){
   // Loop 3: Upon wall being removed, move forwards using distance sensors as guideance.
@@ -122,25 +161,27 @@ void loop() {
   
     // If voltage right is less than voltage left, turn left.
     if (analogRead(rightIRPort) > analogRead(leftIRPort)){
-      digitalWrite(motorRight, HIGH);
+      analogWrite(motorRight, drive);
+      
       delay(duration);
     }
     
     // If volatage left is less than voltage left, turn right.
     else if (analogRead(rightIRPort) < analogRead(leftIRPort)){
-      digitalWrite(motorLeft, HIGH);
+      analogWrite(motorLeft, drive);
+      
       delay(duration);
     }
     
     // If voltages equal, move forwards.
     else{
-      digitalWrite(motorLeft, HIGH);
-      digitalWrite(motorRight, HIGH);
+      analogWrite(motorLeft, drive);
+      analogWrite(motorRight, drive);
       delay(duration);
     }
 
-    digitalWrite(motorLeft, LOW);
-    digitalWrite(motorRight, LOW);
+    analogWrite(motorLeft, 0);
+    analogWrite(motorRight, 0);
     
   }
 }
--- RobotCarCode/test_code/test_code.ino
+++ RobotCarCode/test_code/test_code.ino
@@ -1,12 +1,31 @@
+  int drive = 5;
+  int pause = 0;
+
+  int motor1 = 9;
+  int motor2 = 10;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(5, OUTPUT);
+
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH);
-  digitalWrite(5, HIGH);
+
+  analogWrite(motor1, 51);
+  analogWrite(motor2, 51);
+
+  delay(500);
+
+  analogWrite(motor1, 0);
+
+  delay(500);
+
+  analogWrite(motor2, 0);
+
+  delay(1000);
+  
 }
